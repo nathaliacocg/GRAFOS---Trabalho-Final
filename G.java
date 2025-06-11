856572aa64f6a55489274c2c699a5f457a4cfeff
Nathália de Carvalho Oliveira Cunha Guimarães <96549422+nathaliacocg@users.noreply.github.com>
2025-06-11 19:58:55 -0300

    Add files via upload

---
+++ G.java
@@ -0,0 +1,44 @@
+class V{
+    private String id;
+    private int value;
+
+    public V(){
+        id = "";
+        value = 0;
+    }
+}
+
+class E{
+    private String id;
+    private int value;
+
+    public E(){
+    id = "";
+    value = 0;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public void setValue(int value){
+        this.value = value;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public int getValue(){
+        return value; 
+    }
+
+}
+
+
+public class G {
+    public static void main(String[] args){
+
+    }
+    
+}
