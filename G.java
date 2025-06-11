2aa959c47924a33eada9c68352732b874084aa87
Nathália de Carvalho Oliveira Cunha Guimarães <96549422+nathaliacocg@users.noreply.github.com>
2025-06-11 20:58:33 -0300

    Update G.java

--- G.java
+++ G.java
@@ -1,40 +1,75 @@
-class V{
-    private String id;
-    private int value;
+class V {
+    private int id;
+    private String rotulo; //rotulação
+    private int valor; //ponderação
 
     public V(){
-        id = "";
-        value = 0;
-    }
-}
-
-class E{
-    private String id;
-    private int value;
-
-    public E(){
-    id = "";
-    value = 0;
+        id = 0;
+        valor = 0;
+        rotulo = "";
     }
 
-    public void setId(String id){
+    public void setId(int id){
         this.id = id;
     }
 
-    public void setValue(int value){
-        this.value = value;
+    public void setValor(int valor){
+        this.valor = valor;
     }
 
-    public String getId(){
+    public void setRotulo(String rotulo){
+        this.rotulo = rotulo;
+    }
+
+    public int getId(){
         return id;
     }
 
-    public int getValue(){
-        return value; 
+    public int getValor(){
+        return valor; 
+    }
+
+    public String getRotulo(){
+        return rotulo; 
+    }
+}
+
+
+class E {
+    private int id;
+    private String rotulo; //rotulação
+    private int valor;
+
+    public E(){
+    id = 0;
+    valor = 0;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setValor(int valor){
+        this.valor = valor;
+    }
+
+    public void setRotulo(String rotulo){
+        this.rotulo = rotulo;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public int getValor(){
+        return valor; 
+    }
+
+    public String getRotulo(){
+        return rotulo;
     }
 
 }
-
 
 public class G {
     public static void main(String[] args){
