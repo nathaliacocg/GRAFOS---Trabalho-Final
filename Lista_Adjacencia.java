5e54788913e017daf29e11943926f7955636e120
Nathália de Carvalho Oliveira Cunha Guimarães <96549422+nathaliacocg@users.noreply.github.com>
2025-06-11 20:59:38 -0300

    Add files via upload

---
+++ Lista_Adjacencia.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class Lista_Adjacencia {
+    private int num_v;
+    private LinkedList <Integer>[] lista;
+
+    public Lista_Adjacencia(int num_v){
+        this.num_v = num_v;
+        this.lista = new LinkedList[num_v];
+    }
+
+    public void adicionar_aresta(int origem, int destino){
+        lista[origem].add(destino);
+        lista[destino].add(origem);
+    }
+
+    public void remover_aresta(int origem, int destino){
+        lista[origem].remove(destino);
+        lista[destino].remove(origem);
+    }
+
+    public boolean existe_aresta(int origem, int destino){
+        return lista[origem].contains(destino);
+    }
+
+    public void print_lista(){
+        for(int i = 0; i<num_v; i++){
+            System.out.print("Vértice" + i + ": ");
+            for(Integer adj : lista[i]){
+                System.out.print(adj + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ Matriz_Adjacencia.java
@@ -0,0 +1,36 @@
+public class Matriz_Adjacencia {
+    private int[][] matriz;
+    private int num_v;
+
+    public Matriz_Adjacencia(int num_v){
+        this.num_v = num_v;
+        this.matriz = new int[num_v][num_v];
+    }
+
+    public void adicionar_aresta(int origem, int destino){
+        matriz[origem][destino] = 1;
+        //em um grafo não direcionado, é preciso adicionar as arestas em ambos os sentidos
+        matriz[destino][origem] = 1;
+    }
+
+    public void remover_aresta(int origem, int destino){
+        matriz[origem][destino] = 0;
+        //em um grafo não direcionado, é preciso adicionar as arestas em ambos os sentidos
+        matriz[destino][origem] = 0;
+    }
+
+    public boolean existe_aresta(int origem, int destino){
+        return matriz[origem][destino] == 1;
+    }
+
+    public void print_matriz(){
+        System.out.println("Matriz de Adjacência:");
+        for(int i = 0; i<num_v; i++){
+            for(int j = 0; j<num_v; j++){
+                System.out.println(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
